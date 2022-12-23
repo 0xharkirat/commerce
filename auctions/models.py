2c8666d5324948fbcd5538ceee899460eb61434a
Harkirat Singh <65155920+0xharkirat@users.noreply.github.com>
2022-12-23 14:07:21 +1100

    create bid model

--- auctions/models.py
+++ auctions/models.py
@@ -27,3 +27,12 @@ class Watchlist(models.Model):
     def __str__(self):
         return f"{self.user} -> {self.listing}"
 
+class Bid(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bids")
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="bids")
+    bid = models.DecimalField(max_digits=5, decimal_places=2)
+    isCurrent = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.user}| {self.listing}| {self.bid}| {self.isCurrent}"
+
